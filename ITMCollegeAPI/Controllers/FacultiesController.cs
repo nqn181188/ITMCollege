1e57386188c15381979cd38fc465237e519182bd
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-02-08 23:20:52 +0700

    Faculty-controllerAPI

---
+++ ITMCollegeAPI/Controllers/FacultiesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ITMCollegeAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ITMCollegeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacultiesController : ControllerBase
+    {
+        private readonly ITMCollegeContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public FacultiesController(ITMCollegeContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        // GET: api/Faculties
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Faculty>>> GetFacultys()
+        {
+            return await _context.Facultys.ToListAsync();
+        }
+
+        // GET: api/Faculties/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Faculty>> GetFaculty(int id)
+        {
+            var faculty = await _context.Facultys.FindAsync(id);
+
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(faculty);
+        }
+
+        // PUT: api/Faculties/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFaculty(int id,[FromForm] Faculty faculty)
+        {
+            if (id != faculty.FacultyId)
+            {
+                return BadRequest();
+            }
+            faculty.Image = await SaveImage(faculty.ImageFile);
+            _context.Entry(faculty).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacultyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Faculties
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Faculty>> PostFaculty([FromForm]Faculty faculty)
+        {
+            faculty.Image = await SaveImage(faculty.ImageFile);
+            _context.Facultys.Add(faculty);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Faculties/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFaculty(int id)
+        {
+            var faculty = await _context.Facultys.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            _context.Facultys.Remove(faculty);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FacultyExists(int id)
+        {
+            return _context.Facultys.Any(e => e.FacultyId == id);
+        }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }
+    }
+}
