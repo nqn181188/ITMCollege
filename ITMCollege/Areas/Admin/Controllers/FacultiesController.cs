de8a9c84cf1036a0dffb83c4caccdb82b57917ca
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-02-09 22:08:36 +0700

    SetUpCrudFaculty(20%)

---
+++ ITMCollege/Areas/Admin/Controllers/FacultiesController.cs
@@ -0,0 +1,126 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using ITMCollege.Areas.Client.Controllers;
+using ITMCollege.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ITMCollege.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FacultiesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly INotyfService _notyf;
+
+        private readonly string uri = "http://localhost:20646/api/faculties/";
+        private HttpClient httpclient = new HttpClient();
+
+        public FacultiesController(ILogger<HomeController> logger, INotyfService notyf)
+        {
+            _logger = logger;
+            _notyf = notyf;
+        }
+        // GET: FacultiesController
+        public ActionResult Index()
+        {
+            var model = JsonConvert.DeserializeObject<IEnumerable<Faculty>>(httpclient.GetStringAsync(uri).Result);
+            httpclient.Dispose();
+            return View(model);
+        }
+
+        // GET: FacultiesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = JsonConvert.DeserializeObject<Faculty>(httpclient.GetStringAsync(uri + id).Result);
+            httpclient.Dispose();
+            return View(model);
+        }
+
+        // GET: FacultiesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FacultiesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FacultyId,FalcultyName,Dob,Degree,DepId,Image")] Faculty faculty, IFormFile file)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string file_path = Path.Combine
+                    (Directory.GetCurrentDirectory(), @"wwwroot/Images/Faculty", fileName);
+                using (var stream = new FileStream(file_path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                faculty.Image = "Images/Faculty/" + fileName;
+                var data = httpclient.PostAsJsonAsync<Faculty>(uri, faculty).Result;
+                if (data.IsSuccessStatusCode)
+                {
+                    _notyf.Success("Create Succesfully");
+                    httpclient.Dispose();
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FacultiesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FacultiesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FacultiesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FacultiesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- ITMCollege/Models/Faculty.cs
+++ ITMCollege/Models/Faculty.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +11,16 @@ namespace ITMCollege.Models
     public class Faculty
     {
         public int FacultyId { get; set; }
+        [Display(Name ="Name")]
         public string FalcultyName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Dob { get; set; }
         public string Degree { get; set; }
         public int DepId { get; set; }
         public string Image { get; set; }
 
-        [NotMapped]
-        public IFormFile ImageFile { get; set; }
+        //[NotMapped]
+        //public IFormFile ImageFile { get; set; }
         public virtual Department Dep { get; set; }
     }
 }
