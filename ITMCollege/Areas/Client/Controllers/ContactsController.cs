1913a6bbf0c3c3003feb816c586b9647c0e75dd6
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-02-11 16:46:10 +0700

    Customer Contact Us Page

---
+++ ITMCollege/Areas/Client/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMCollege.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class ContactsController : Controller
+    {
+        // GET: ContactsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
