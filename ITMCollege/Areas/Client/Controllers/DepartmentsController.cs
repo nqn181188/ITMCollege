51709c5f1a627331bf3ee958b68bd2ed2ef1764d
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-02-11 20:30:41 +0700

    Customer Department Page

---
+++ ITMCollege/Areas/Client/Controllers/DepartmentsController.cs
@@ -0,0 +1,97 @@
+﻿using ITMCollege.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ITMCollege.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class DepartmentsController : Controller
+    {
+        private readonly string uri = "http://localhost:20646/api/departments/";
+        private HttpClient httpclient = new HttpClient();
+        // GET: DepartmentsController
+        public ActionResult Index()
+        {
+            var model = JsonConvert.DeserializeObject<IEnumerable<Department>>(httpclient.GetStringAsync(uri).Result);
+            httpclient.Dispose();
+            return View(model);
+        }
+
+        // GET: DepartmentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = JsonConvert.DeserializeObject<Department>(httpclient.GetStringAsync(uri + id).Result);
+            httpclient.Dispose();
+            return View(model);
+        }
+
+        // GET: DepartmentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
