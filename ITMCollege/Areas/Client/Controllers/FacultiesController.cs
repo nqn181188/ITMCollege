9aa6d4c1e01166c56d56020a605204423440b10b
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-02-15 20:14:04 +0700

    add pagination Department,Faculty,Facility

--- ITMCollege/Areas/Client/Controllers/FacultiesController.cs
+++ ITMCollege/Areas/Client/Controllers/FacultiesController.cs
@@ -17,12 +17,21 @@ namespace ITMCollege.Areas.Client.Controllers
         private readonly string uri2 = "http://localhost:20646/api/departments/";
         private HttpClient httpclient = new HttpClient();
         // GET: FacultiesController
-        public ActionResult Index()
+        public ActionResult Index(int pg=1)
         {
             var model = JsonConvert.DeserializeObject<IEnumerable<Faculty>>(httpclient.GetStringAsync(uri).Result);
             ViewBag.listdep = JsonConvert.DeserializeObject<IEnumerable<Department>>(httpclient.GetStringAsync(uri2).Result);
             httpclient.Dispose();
-            return View(model);
+            const int pageSize = 6;
+            if (pg < 1)
+                pg = 1;
+            int rescCount = model.Count();
+            var pager = new Pager(rescCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = model.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            //return View(model);
+            return View(data);
         }
 
         // GET: FacultiesController/Details/5
