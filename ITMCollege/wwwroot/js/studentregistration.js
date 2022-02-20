13219ab0ac0f7cf95c55bf301f008999c12549cf
nguyen1811 <76578816+rhythmhouse@users.noreply.github.com>
2022-02-20 13:38:55 +0700

    Registration

---
+++ ITMCollege/wwwroot/js/studentregistration.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#registrationForm").hide();
+});
+$("#btnSubmit").click(function (e) {
+    e.preventDefault();
+    $("#regNumErr").empty();
+    $("#regnum").removeClass("input-err");
+    var regNum = $("#regnum").val();
+    if (regNum == "") {
+        $("#registrationForm").hide();
+        $("#regnum").addClass("input-err");
+        $("#regNumErr").append("Registration Number cannot be blank.");
+    } else {
+        var regex = /^ST\d{8,8}*$/;
+        if (!regex.test(regNum)) {
+            $("#registrationForm").hide();
+            $("#regnum").addClass("input-err");
+            $("#regNumErr").append("Registration Number must follow the pattern STxxxxxxxx. Example : ST12345678");
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "/Client/Registrations/GetAdmissionInfor",
+                data: { 'regNum': regNum },
+
+
+            });
+        }
+    }
+});
